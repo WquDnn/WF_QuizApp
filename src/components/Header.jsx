2714b3eb7aeda642b48eb43d120a45d221aca4d4
WquDnn <wqulbgordynska@gmail.com>
2025-11-30 15:45:52 +0200

    asdfdasffadsadsfdsfadfas

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtLXqHb-IcHwX23PRhIHo7cgUaLaOqEwtRAA&s" alt="logo" />
+            <h1>TryToEatDeadlyMushroom</h1>
+        </header>
+      </div>
+    )
+  }
+}
